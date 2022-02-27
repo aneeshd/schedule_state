d8ed6567e63f4747f9260aeef5cca8b130d2302b
Aneesh Dalvi <2533133+aneeshd@users.noreply.github.com>
2022-02-27 15:10:39 -0500

    Potential fix for issue #4
    Wait to install callbacks until async_added_to_hass

--- custom_components/schedule_state/sensor.py
+++ custom_components/schedule_state/sensor.py
@@ -171,12 +171,15 @@ class ScheduleSensor(SensorEntity):
 
     def __init__(self, hass, data, name, default_state):
         """Initialize the sensor."""
-        self._hass = hass
         self.data = data
         self._attributes = {}
         self._name = name
         self._state = None
         self._default_state = default_state
+
+    async def async_added_to_hass(self):
+        """Handle added to Hass."""
+        await super().async_added_to_hass()
 
         @callback
         def recalc_callback(*args):
@@ -190,7 +193,7 @@ class ScheduleSensor(SensorEntity):
             )
             self.async_on_remove(
                 async_track_state_change_event(
-                    self._hass, self.data.entities, recalc_callback
+                    self.hass, self.data.entities, recalc_callback
                 )
             )
 
